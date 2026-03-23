e7b9050696db1c69865742baaba7379880d7dea8
egarci000 <115583629+egarci000@users.noreply.github.com>
2026-03-23 17:14:57 -0500

    Add completed README file and completed solution.sql file for Intermediate/ex_02

--- LearningSQL/Exercises/Intermediate/ex02_refining_selections/README.md
+++ LearningSQL/Exercises/Intermediate/ex02_refining_selections/README.md
@@ -2,6 +2,14 @@
 
 
 
+Using terms to refine selections in this section of the course, as well as past knowledge learned in previous sections, I solve the following exercises.
+
+
+
+Things learned in this section: DISTINCT, LIKE, Wildcards (% and \_), ORDER BY, LIMIT
+
+
+
 1. Select all story collections. Titles that contain the string, 'stories'
 2. Find the longest book by page count. Print out title and page count. Find using only things learning in refining selections section, not keywords like MAX.
 3. Print a summary containing the title and year, for the 3 most recent books. Example format: 'Book title - released\_year', etc.
---
+++ LearningSQL/Exercises/Intermediate/ex02_refining_selections/solution.sql
@@ -0,0 +1,54 @@
+-- 1. Select all story collections. Titles that contain the string, 'stories'
+
+-- Use % wildcard
+SELECT title FROM books
+WHERE title LIKE '%stories%';
+
+
+-- 2. Find the longest book by page count. Print out title and page count. 
+-- Find using only things learning in refining selections section, not keywords like MAX.
+
+-- Using Order By and Limit we can achieve this
+SELECT title, pages FROM books
+ORDER BY pages DESC
+LIMIT 1;
+
+
+-- 3. Print a summary containing the title and year, for the 3 most recent books. 
+-- Example format: 'Book title - released_year', etc.ALTER
+
+-- Concat then order by released year in descending order and limit results to 3
+SELECT CONCAT(title, ' - ', released_year) FROM books
+ORDER BY released_year DESC
+LIMIT 3;
+
+
+-- 4. Find all books with an author last name that contains a space (' '). Return title and author_lname
+
+-- Straighforward, use % wildcard
+SELECT title, author_lname FROM books 
+WHERE author_lname LIKE '% %';
+
+
+-- 5. Find the 3 books with the lowest stock available. Select title, year, and stock
+
+-- Use ORDER by stock_quantity using default Ascending order. Limit to 3
+SELECT title, released_year, stock_quantity
+ORDER BY stock_quantity
+LIMIT 3;
+
+
+-- 6. Print title and author last name, sorted first by author last name then title
+
+-- Use Order By
+SELECT title, author_lname FROM books
+ORDER BY author_lname, title;
+
+
+-- 7. Print out the example format for every book in the table:
+ -- 'MY FAVORITE AUTHOR IS [AUTHOR FIRST NAME] [AUTHOR LAST NAME]' sorted alphabetically by last name with the alias of 'yell'
+
+-- Use UPPER with CONCAT then Order by author last name
+SELECT UPPER(CONCAT('My favorite author is ', author_fname, ' ', author_lname)) as yell FROM books
+ORDER BY author_lname;
+
